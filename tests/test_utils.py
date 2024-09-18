96f0bcbc3e02bb91093597a27164fa240ea9f811
Marco Pizzocaro <m.pizzocaro@inrim.it>
2024-09-18 18:03:39 +0200

    Add first test for utils

---
+++ tests/test_utils.py
@@ -0,0 +1,18 @@
+import numpy as np
+import pytest
+from hypothesis import given
+from hypothesis import strategies as st
+from hypothesis.extra.numpy import arrays
+
+from gauss_markov_srs.utils import corr2cov, cov2corr
+
+
+@given(arrays(np.float64, (5, 5), elements=st.floats(min_value=1.0, max_value=10.0)))
+def test_cov2corr_and_corr2cov_inversion(cov):
+    # Ensure cov is symmetric and positive definite
+    cov = np.dot(cov, cov.T)
+
+    corr, std = cov2corr(cov, return_std=True)
+    cov_reconstructed = corr2cov(corr, std)
+
+    pytest.approx(cov, cov_reconstructed)
